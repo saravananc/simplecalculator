4e908615266ba03ef78434b107d1b78ce9b9a4da
saravananc <csaravananit@gmail.com>
2023-07-24 19:28:54 +0530

    SimpleCalculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Col, FormControl, Row } from 'react-bootstrap';
 
-function App() {
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "*"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+
+
+const  App =() => {
+
+  const [inputValue, setInputValue] = useState('');
+
+  const handleButtonClick = (value) => {
+    if (value === '=') {
+      try {
+        setInputValue(eval(inputValue).toString());
+      } catch (error) {
+        setInputValue('Error');
+      }
+    } else if (value === 'C') {
+      setInputValue('');
+    } else if (value === '+-') {
+      setInputValue((prevValue) => (parseFloat(prevValue) * -1).toString());
+    } else if (value === '%') {
+      setInputValue((prevValue) => (parseFloat(prevValue) / 100).toString());
+    } else {
+      setInputValue((prevValue) => prevValue + value);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+     <div className="calculator-container mt-5">
+      <div className="calculator">
+        <FormControl
+          type="text"
+          className="form-control"
+          value={inputValue}
+          readOnly
+        />
+
+        {btnValues.map((row, rowIndex) => (
+          <Row key={rowIndex}>
+            {row.map((value, colIndex) => (
+              <Col key={colIndex} >
+                <Button
+                  variant="outline-primary"
+                  onClick={() => handleButtonClick(value)}
+                  style={{ height: '100%' }}
+                >
+                  {value}
+                </Button>
+              </Col>
+            ))}
+          </Row>
+        ))}
+      </div>
     </div>
+    </>
   );
 }
 
 export default App;
+
